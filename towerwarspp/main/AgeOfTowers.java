d66bdb1c8e94e9504c95813df14fab228cd6e73a
niklas.mueller01 <niklas.mueller01@stud.uni-goettingen.de>
2017-06-22 18:47:22 +0000

    Erweiterte Main-Klasse: -getMove() Methode -debugMode -delayMode
    M    towerwarspp/main/AgeOfTowers.java

--- towerwarspp/main/AgeOfTowers.java
+++ towerwarspp/main/AgeOfTowers.java
@@ -2,10 +2,11 @@ package towerwarspp.main;
 
 import towerwarspp.preset.ArgumentParser;
 import towerwarspp.preset.ArgumentParserException;
+import towerwarspp.io.*;
 
 /**
- * Class {@link ageoftowers} initializing a new game of TowerWarsPP with given settings <br>
- *     from the commandline using the ArgumentParser.
+ * Class {@link AgeOfTowers} initializing a new game of TowerWarsPP with given settings <br>
+ *     from the commandline using the {@link ArgumentParser}.
  *
  * @version 0.1 20th June 2017
  * @author Niklas Mueller
@@ -17,6 +18,11 @@ public class AgeOfTowers {
      * @param args list with settings to put into the ArgumentParser
      */
     public AgeOfTowers(String[] args) {
+
+        boolean debugMode;
+        int delayTime;
+        //<Board> board
+
         try {
             ArgumentParser ap = new ArgumentParser(args);
 
@@ -78,18 +84,32 @@ public class AgeOfTowers {
                     <AdvancedPlayer> blue = new <AdvancedPlayer>();
                      */
             }
+            
+            /*set wether it's should be debug mode or not */
+            debugMode = ap.isDebug();
 
+            /*set delay-time*/
+            delayTime = ap.getDelay();
             //
         }
         catch (ArgumentParserException e) {
             e.printStackTrace();
         }
 
+        /*Object from wich implements requestable-interface*/
+        //textInput input = new textInput();
+
         red.init(ap.getSize(), PlayerColor.RED);
         blue.init(ap.getSize(), PlayerColor.BLUE);
 
+        Player redPlayer = red;
+        Player bluePlayer = blue;
 
         /*initialize board object with given size */
+        //board = new <Board>(ap.getSize());
+
+        /*initialize viewer-object with method viewer() from board*/
+        //<Viewer> viewer = board.viewer();
 
 
         /*
@@ -102,13 +122,110 @@ public class AgeOfTowers {
 
           output current gamestatus (graphic/textual)
 
-          tell output if move is leading to end the game
+          tell output if move is leading to end of game
 
           possibility to play with a network player
 
          */
+
+
+        Status status = OK;
+        Move redMove = null;
+        Move blueMove = null;
+
+        while (true) {
+            /*red's move*/
+            try {
+                /*request move*/
+                redMove = redPlayer.request();
+
+                /*confirm move */
+                redPlayer.confirm(board.getStatus());
+
+                /*update move*/
+                redPlayer.update(blueMove, board.getStatus());
+
+                if (board.getStatus() == Status.RED_WIN) {
+                    status = RED_WIN;
+                    break;
+                }
+
+                /*make move on board*/
+                //board.makeMove(redMove);
+
+                /*if debug-mode is enabled*/
+                if (debugMode) {
+                    System.out.println("Red's move: " + redMove.toString());
+                    //output board
+                    System.out.println("Status: " + redMove.getStatus());
+                }
+
+                /*if delay-mode is enabled*/
+                if (delayTime != 0) {
+                    Thread.sleep(delayTime);
+                }
+            }
+            catch (RemoteException e) {
+                e.printStackTrace();
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            /*blue's move*/
+            try {
+                /*update move*/
+                bluePlayer.update(redMove, board.getStatus());
+
+                /*request move*/
+                blueMove = bluePlayer.request();
+
+                /*confirm move */
+                bluePlayer.confirm(board.getStatus());
+
+                if (board.getStatus() == Status.BLUE_WIN) {
+                    status = BLUE_WIN;
+                    break;
+                }
+
+                /*make move on board*/
+                //board.makeMove(blueMove);
+                
+                /*if debug-mode is enabled*/
+                if (debugMode) {
+                    System.out.println("Blue's move: " + blueMove.toString());
+                    //output board
+                    System.out.println("Status: " + blueMove.getStatus());
+                }
+
+                /*if delay-mode is enabled*/
+                if (delayTime != 0) {
+                    Thread.sleep(delayTime);
+                }
+            }
+            catch (RemoteException e) {
+                e.printStackTrace();
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        /*end of game */
     }
 
+    /**
+     * Method getMove calling deliver from input-class to get a move from user input
+     * @return move parsed from user input
+     */
+    public Move getMove() {
+        //return inputClass.deliver();
+    }
+
+    /**
+     * Main method initializing a new object of AgeOfTowers parsing the input String
+     * @param args input specifying all settings
+     */
     public static void main(String[] args) {
         AgeOfTowers game = new AgeOfTowers(args);
     }
