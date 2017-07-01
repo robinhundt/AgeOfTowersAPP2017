f6f9af2414cf8d11db5208b66a3c00ca6c1b66cf
anastasiia.kysliak <anastasiia.kysliak@stud.uni-goettingen.de>
2017-07-01 17:29:56 +0000

    BViewer added.

---
+++ src/towerwarspp/board/BViewer.java
@@ -0,0 +1,75 @@
+package towerwarspp.board;
+
+import towerwarspp.preset.*;
+import towerwarspp.io.*;
+import static towerwarspp.preset.PlayerColor.*;
+import static towerwarspp.preset.Status.*;
+import java.util.Vector;
+import java.util.ListIterator;
+import java.lang.Exception;
+
+public class BViewer implements Viewer {
+	private SimpleBoard boardO;
+	private Entity[][] board;
+	private int size;
+
+	public BViewer(SimpleBoard b, Entity[][] ar, int sz) {
+		boardO = b;
+		board = ar;
+		size = sz;
+	}
+	
+    	public int getSize() {
+		return size; 
+	}
+	public Status getStatus() {
+		return boardO.getStatus();
+	}
+	public int getTurn() {
+		return boardO.getTurn();
+	}
+    	public Move[] getPossibleMoves(Position position) {
+		return new Move[2];
+	}
+
+	/**
+	* Creates and returns a new {@link MoveList} object with all possible moves
+	* which a figure (stone, tower, base) on the position {@link p} has
+	* @param p - position of the figure.
+	* @return
+	*	a {@link MoveList} with all possible moves which the specified figure has.
+	*/
+	public MoveList possibleMoves(Position p) throws Exception {
+		Entity ent = board[p.getLetter()][p.getNumber()];
+		if (ent == null) throw new Exception ("Position is empty");
+		return new MoveList(ent);
+	}
+	public boolean isTower(Position pos) {
+		Entity ent = getElement(pos);
+		return (ent != null && ent.isTower());
+	}
+	public boolean isStone(Position pos) {
+		Entity ent = getElement(pos);
+		return (ent != null && !ent.isBase() && !ent.isTower());
+	}
+	public boolean isBlocked(Position pos) {
+		Entity ent = getElement(pos);
+		return (ent != null && !ent.isBlocked());
+	}
+	public int getHeight(Position pos) {
+		Entity ent = getElement(pos);
+		return ent.getHigh();
+	}
+    	public PlayerColor getPlayerColor(Position pos) {
+		Entity ent = getElement(pos);
+		return ent.getColor();
+	}
+	/**
+	* Returns an element located on the specified position on the board.
+	* @param pos the position of the element that has to be returned.
+	* @return an element located on the specified position on the board.
+	*/
+	private Entity getElement(Position pos) {
+		return board[pos.getLetter()][pos.getNumber()];
+	}
+}
