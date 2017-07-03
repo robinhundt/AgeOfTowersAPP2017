12ebe0b484d7cda4edfebc4e147d8deb73efe754
robinwilliam.hundt <robinwilliam.hundt@stud.uni-goettingen.de>
2017-07-03 19:26:22 +0000

    Test implementation of RndPlayer. Might not be working. Testing needed.

--- src/towerwarspp/player/RndPlayer.java
+++ src/towerwarspp/player/RndPlayer.java
@@ -1,8 +1,10 @@
 package towerwarspp.player;
 
+import towerwarspp.board.MoveList;
 import towerwarspp.preset.Move;
 
 import java.rmi.RemoteException;
+import java.util.Iterator;
 import java.util.Random;
 import java.util.Vector;
 
@@ -19,9 +21,22 @@ class RndPlayer extends BasePlayer {
 
     @Override
     Move deliverMove() throws Exception {
-        Vector<Move> moves = board.allPossibleMoves(color);
-        Move move = moves.get(rnd.nextInt(moves.size()));
+        Vector<MoveList> moveLists = board.getAllPossibleMoves(color);
+        MoveList moveList = moveLists.get(rnd.nextInt(moveLists.size()));
+        Move move = rndMove(moveList);
+        while (move == null)
+            move = rndMove(moveList);
         board.update(move, color);
+        return move;
+    }
+
+    private Move rndMove(MoveList moveList) {
+        Move move = null;
+        Iterator<Move> it = moveList.iterator();
+
+        for(int i=0; i<moveList.size(); i++)
+            move = it.next();
+
         return move;
     }
 }
