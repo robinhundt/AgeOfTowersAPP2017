6f2027623bfff3ea7a8eeea5f15fc1cda89676da
niklas.mueller01 <niklas.mueller01@stud.uni-goettingen.de>
2017-07-04 10:28:58 +0000

    main -- added option for tournament mode, may not be working, switching of players needs to be added help option updated

--- src/towerwarspp/main/AgeOfTowers.java
+++ src/towerwarspp/main/AgeOfTowers.java
@@ -16,38 +16,72 @@ public class AgeOfTowers {
         Player firstPlayer, secondPlayer;
         IO io;
         try {
+            /*create new ArgumentParse object*/
             ArgumentParser ap = new ArgumentParser(args);
 
+            /*check if no parameters have been given, or flag --help is activated*/
             if (args.length == 0 || ap.isHelp()) {
                 System.out.println(helpOutput());
                 System.exit(0);
             }
 
+            /*check if board size is valid*/
+            if (ap.getSize() < 4 || ap.getSize() > 26) {
+                System.out.println("Board size needs to be between 4 and 26!");
+                System.exit(1);
+            }
+
+            /*create new board and viewer object*/
             Board board = new Board(ap.getSize());
             BViewer viewer = board.viewer();
 
-//            if (ap.isGraphic()) {
-//                //io = new GraphicIO(viewer);
-//            }
-//            else {
+            /*check if graphic output should be enabled*/
+            if (ap.isGraphic()) {
                 io = new TextIO(viewer);
-//            }
+            }
+            /*otherwise do output on standard output*/
+            else {
+                io = new TextIO(viewer);
+            }
 
-//            if (ap.isSet("red")) {
-            //TODO player one and two instead of blue and red
-                firstPlayer = makePlayer(ap.getSize(), RED, ap.getRed(), (Requestable) io);
-//            }
-//            if (ap.isSet("blue")) {
+            /*check if red and blue player types are given, then initialize them*/
+            if (ap.isSet("red")) {
+                firstPlayer = makePlayer(ap.getSize(), RED, ap.getRed(), io);
+            }
+            else {
+                firstPlayer = null;
+            }
+            if (ap.isSet("blue")) {
                 secondPlayer = makePlayer(ap.getSize(), BLUE, ap.getBlue(), (Requestable) io);
-//            }
+            }
+            else {
+                secondPlayer = null;
+            }
+
+            /*check if tournament mode is enabled*/
+            int rounds = 1;
+            if (ap.isSet("rounds")) {
+                rounds = ap.getRounds();
+            }
+
+            /*create String to store statistics*/
+            String allResults = "";
 
 
-            Game game = new Game(firstPlayer, secondPlayer, OutputType.TEXTUAL, true,
-                    ap.isSet("delay") ? ap.getDelay() : 0, ap.getSize());
+            /*start as many games as wished*/
+            for (int i=1; i<=rounds; i++) {
+                if (rounds > 1) System.out.println("Round No.: " + i);
+                /*create a new game object with the given players and settings*/
+                Game game = new Game(firstPlayer, secondPlayer, OutputType.TEXTUAL, true,
+                        ap.isSet("delay") ? ap.getDelay() : 0, ap.getSize());
 
-            Result result = game.play();
+                /*start game and save game result in result object*/
+                Result result = game.play();
+                allResults += "Round " + i + ": " + result.toString() + "\n";
+            }
 
-            System.out.println(result);
+            /*output game result*/
+            System.out.println(allResults);
         }
         catch (ArgumentParserException e) {
             System.out.println(e);
@@ -57,20 +91,27 @@ public class AgeOfTowers {
         }
     }
 
+
     private static String helpOutput() {
         return  "Starts a new game of TowerWarsPP with the given settings. \n" +
                 "USAGE: \n --help \tshows this help message \n" +
                 "Settings start with '-' and need a parameter and can be set as followed. \n" +
-                "Flags start with '--' and do not need parameter. \n" +
-                "\nsettings: \n" +
+                "Flags start with '--' and do not need parameter. All flags are optional.\n" +
+                "\nSETTINGS: \n" +
+                "obligatory:\n" +
                 "-red \t chose player type for red \n" +
                 "-blue \t chose player type for blue \n" +
                 "-size \t chose board size \n" +
-                "-rounds \t set number of rounds for the tournament \n" +
-                "-delay \t set delay time in milliseconds to slow down the game \n" +
-                "\nflags: \n" +
+                "optional:\n" +
+                "-rounds \t activates tournament mode and sets number of rounds for the tournament \n" +
+                "-delay \t sets delay time in milliseconds to slow down the game \n" +
+                "possible parameter:\n" +
+                "player types: human, random, simple, adv1, remote \n" +
+                "size: integer between 4 and 26 \n" +
+                "rounds: integer bigger than 1 \n" +
+                "delay: integer bigger than 0 (in milliseconds) \n" +
+                "\nFLAGS: \n" +
                 "--graphic \t activates the graphic output, if not set output will be on the standard-output \n" +
-                "--debug \t activates the debug-mode: shows additional information for every move \n" +
-                "--tournament \t activates the tournament-mode, in addition, '-rounds' needs to be set ";
+                "--debug \t activates the debug-mode: shows additional information for every move \n";
     }
 }
