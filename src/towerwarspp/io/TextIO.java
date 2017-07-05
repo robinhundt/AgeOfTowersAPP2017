94bcdc10fc91369d34b51aedc4e265aa1edbc994
niklas.mueller01 <niklas.mueller01@stud.uni-goettingen.de>
2017-07-05 09:03:31 +0000

    removed redundant method from TextIO

--- src/towerwarspp/io/TextIO.java
+++ src/towerwarspp/io/TextIO.java
@@ -43,74 +43,12 @@ public class TextIO implements IO {
         this.scanner = new Scanner(System.in);
     }
 
-    /**
-     * just for testing
-     */
-    public TextIO() {}
-
-    /**
-     * Checks if is Stone
-     * @param x X-Coordinate
-     * @param y Y-Coordinate
-     * @return  True if Token is stone, otherwise false
-     */
-    private boolean isStone(int x, int y) {
-        return this.viewer.isStone(new Position(x, y));
-    }
-
-    /**
-     * Checks if Stone is Tower
-     * @param x X-Coordinate
-     * @param y Y-Coordinate
-     * @return True if Stone is Tower, otherwise false
-     */
-    private boolean isTower(int x, int y) {
-        return this.viewer.isTower(new Position(x, y));
-    }
-
-    /**
-     * Checks if Stone is blocked
-     * @param x X-Coordinate
-     * @param y Y-Coordinate
-     * @return True if Stone is vlocked, otherwise false
-     */
-    private boolean isBlocked(int x, int y) {
-        return this.viewer.isBlocked(new Position(x, y));
-    }
-
-    /**
-     * Get the height of a Tower
-     * @param x X-Coordinate
-     * @param y Y-Coordinate
-     * @return height of Tower
-     */
-    private int getTowerHeight(int x, int y) {
-        return this.viewer.getHeight(new Position(x, y));
-    }
-
-    /**
-     * Get owner of Tower
-     * @param x X-Coordinate
-     * @param y Y-Coordinate
-     * @return Playercolor
-     */
-    private PlayerColor getPlayerColor(int x, int y) {
-        return this.viewer.getPlayerColor(new Position(x, y));
-    }
-
-    /**
-     * Get the size of the Board
-     * @return Size of Board
-     */
-    private int getSize() {
-        return this.viewer.getSize();
-    }
 
     /**
      * Output of the Board
      */
     public void visualize() {
-        int size = getSize();
+        int size = this.viewer.getSize();
         char headChar = 'A';
         System.out.print("    ");
         for(int top = 0; top < size; ++top) {
@@ -122,26 +60,26 @@ public class TextIO implements IO {
         for(int y = 1; y <= size; ++y) {//!!!!
             System.out.print(tap + y + ((y>=10) ? " " : "  "));
             for(int x = 1; x <= size; ++x) {//!!!!!
-                if(isTower(x, y)) {
-                    if(getPlayerColor(x, y) == PlayerColor.RED) {
+                if(this.viewer.isTower(new Position(x, y))) {
+                    if(this.viewer.getPlayerColor(new Position(x, y)) == PlayerColor.RED) {
                         System.out.print(RED);
                     } else {
                         System.out.print(BLUE);
                     }
-                    System.out.print(" T" + getTowerHeight(x, y) + RESET);
+                    System.out.print(" T" + this.viewer.getHeight(new Position(x, y)) + RESET);
                 } else if(x == 1 && y == 1) {
                     System.out.print(RED + " B " + RESET);
                 } else if(x == size && y == size) {
                     System.out.print(BLUE + " B " + RESET);
-                } else if(isStone(x, y)) {
-                    if(getPlayerColor(x, y) == PlayerColor.RED) {
+                } else if(this.viewer.isStone(new Position(x, y))) {
+                    if(this.viewer.getPlayerColor(new Position(x, y)) == PlayerColor.RED) {
                         System.out.print(RED);
                     } else {
                         System.out.print(BLUE);
                     }
                     System.out.print(" S " + RESET);
-                } else if(isBlocked(x, y)) {
-                    if(getPlayerColor(x, y) == PlayerColor.RED) {
+                } else if(this.viewer.isBlocked(new Position(x, y))) {
+                    if(this.viewer.getPlayerColor(new Position(x, y)) == PlayerColor.RED) {
                         System.out.print(RED);
                     } else {
                         System.out.print(BLUE);
