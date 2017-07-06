d1f781bb212ee5d9c7e0d9aa5fb6adefee91ff89
robinwilliam.hundt <robinwilliam.hundt@stud.uni-goettingen.de>
2017-07-06 14:39:55 +0000

    Added setViewer(Viewer viewer) method to View Interface

--- src/towerwarspp/io/View.java
+++ src/towerwarspp/io/View.java
@@ -1,8 +1,12 @@
 package towerwarspp.io;
 
+import towerwarspp.preset.Viewer;
+
 /**
  * Created by robin on 04.07.17.
  */
 public interface View {
     public void visualize();
+
+    public void setViewer(Viewer viewer);
 }
