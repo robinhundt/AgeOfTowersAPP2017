6ff92d334b9efcd8f3b14a1b60b8075959b21a8f
robinwilliam.hundt <robinwilliam.hundt@stud.uni-goettingen.de>
2017-07-09 15:13:41 +0000

    Added AdvPlayer

--- src/towerwarspp/player/AdvPlayer.java
+++ src/towerwarspp/player/AdvPlayer.java
@@ -1,16 +1,51 @@
 package towerwarspp.player;
 
+import towerwarspp.board.Board;
+import towerwarspp.io.View;
+import towerwarspp.player.ai.SimRndGame;
 import towerwarspp.preset.Move;
-
-import java.rmi.RemoteException;
 
 /**
  * Created by robin on 23.06.17.
  */
-class AdvPlayer extends BasePlayer {
+public class AdvPlayer extends BasePlayer {
+    private final int simCount;
+
+    public AdvPlayer(int simCount) {
+        this.simCount = simCount;
+    }
+
+    public AdvPlayer(View view, int simCount) {
+        this(simCount);
+        setView(view);
+    }
 
     @Override
     Move deliverMove() throws Exception {
-        return null;
+        Move bestMove = null;
+        int bestWinCounter = -1;
+
+        for(Move move : board.allPossibleMoves(color)) {
+            int winCount = 0;
+            for(int i=0; i < simCount; i++) {
+                SimRndGame sim = new SimRndGame(board.clone(), move, color);
+                if(sim.simulate())
+                    winCount++;
+            }
+            if(winCount > bestWinCounter)
+                bestMove = move;
+
+        }
+        return bestMove;
     }
+
+//        System.out.println("Before clone");
+//        for (Move move : board.allPossibleMoves(color))
+//            System.out.println(move);
+//
+//        System.out.println("After clone");
+//        Board copy = board.clone();
+//        for (Move move : copy.allPossibleMoves(color))
+//            System.out.println(move);
+//        return null;
 }
