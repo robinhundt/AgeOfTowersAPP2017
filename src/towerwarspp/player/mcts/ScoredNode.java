5a8c32ab43d1ae3d413debae536494b785ff24a4
robinwilliam.hundt <robinwilliam.hundt@stud.uni-goettingen.de>
2017-07-16 13:01:50 +0000

    Added comments for ScoredNode

--- src/towerwarspp/player/mcts/ScoredNode.java
+++ src/towerwarspp/player/mcts/ScoredNode.java
@@ -1,17 +1,34 @@
 package towerwarspp.player.mcts;
 
 /**
- * Created by robin on 14.07.17.
+ * Simple Data container class that contains pairs of Nodes and their score. Implements {@link Comparable<ScoredNode>}
+ * to impose a total ordering. Scored Nodes are order by the natural ordering of their scores.
  */
 class ScoredNode implements Comparable<ScoredNode>{
+    /**
+     * Contained Node in pair.
+     */
     final Node node;
+    /**
+     * Contained UCB1 score in Pair.
+     */
     final double ucbScore;
 
+    /**
+     * Construct a new pair that holds a reference to the passed Node and it's  score.
+     * @param node
+     * @param ucbScore
+     */
     ScoredNode(Node node, double ucbScore) {
         this.node = node;
         this.ucbScore = ucbScore;
     }
 
+    /**
+     * Imposes a total ordering on scored Nodes equivalent to the ordering of their double scores.
+     * @param scoredNode
+     * @return
+     */
     @Override
     public int compareTo(ScoredNode scoredNode) {
         if(ucbScore < scoredNode.ucbScore)
