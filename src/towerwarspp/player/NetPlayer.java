20e699513421a55792e65525e3f698edf5fcd1bc
robinwilliam.hundt <robinwilliam.hundt@stud.uni-goettingen.de>
2017-07-16 19:00:31 +0000

    Fixed JavaDoc issues

--- src/towerwarspp/player/NetPlayer.java
+++ src/towerwarspp/player/NetPlayer.java
@@ -13,7 +13,7 @@ import java.rmi.server.UnicastRemoteObject;
  * Implementation of the {@link Player} Interface that can be used to offer any of the other {@link towerwarspp.preset.PlayerType}
  * on a local RMI. During the  constructor call a {@link Player} object has to be passed. Calling the {@link #request()},
  * {@link #confirm(Status)}, {@link #update(Move, Status)} and {@link #init(int, PlayerColor)} methods on a NetPlayer will
- * call the equivalent methods on the NetPlayers {@link #player} instance.
+ * call the equivalent methods on the NetPlayers {@link Player} instance.
  * As with sub classes of {@link BasePlayer} request() - confirm() and makeMove() must be called in the correct order.
  * @author Alexander Wähling
  * @version 08-07-17
@@ -22,7 +22,7 @@ public class NetPlayer extends UnicastRemoteObject implements Player {
     /**
      * {@link Player} instance to which this NetPlayer is connected. All method calls on this NetPlayer will invoke the
      * corresponding calls on this Player instance. E.g. if {@link #request()} is called on a NetPlayer, the netPlayer will
-     * call {@link #player#request()}.
+     * call {@link Player#request()}.
      */
     private Player player;
     private View view;
@@ -50,8 +50,8 @@ public class NetPlayer extends UnicastRemoteObject implements Player {
     }
 
     /**
-     * Calling the request() method will invoke a {@link #player#request()} call.
-     * @return Move returned by {@link #player#request()}
+     * Calling the request() method will invoke a {@link Player#request()} call.
+     * @return Move returned by {@link Player#request()}
      * @throws Exception if request() is not called after {@link #init(int, PlayerColor)} or {@link #update(Move, Status)}
      */
     @Override
@@ -63,9 +63,9 @@ public class NetPlayer extends UnicastRemoteObject implements Player {
     }
 
     /**
-     * Calling confirm() method will invoke a {@link #player#confirm(Status)} call.
+     * Calling confirm() method will invoke a {@link Player#confirm(Status)} call.
      * @param boardStatus {@link Status} to confirm Players own status with.
-     * @throws Exception Throws an Exception in case the method is invoked in the wrong order, or {@link #player}'s board
+     * @throws Exception Throws an Exception in case the method is invoked in the wrong order, or {@link Player}'s board
      * status differs from passed one, or is {@link Status#ILLEGAL}.
      */
     @Override
@@ -74,7 +74,7 @@ public class NetPlayer extends UnicastRemoteObject implements Player {
     }
 
     /**
-     * Calling makeMove() will invoke a {@link #player#update(Move, Status)} call.
+     * Calling makeMove() will invoke a {@link Player#update(Move, Status)} call.
      * @param opponentMove opponents move to place on own Board
      * @param boardStatus opponents board status after Move to validate against own status
      * @throws Exception is thrown if method is called in wrong order, or status is ILLEGAL or different from passed status
@@ -87,11 +87,11 @@ public class NetPlayer extends UnicastRemoteObject implements Player {
     }
 
     /**
-     * Method to initialize current NetPlayer. calling it will invoke {@link #player#init(int, PlayerColor)}.
+     * Method to initialize current NetPlayer. calling it will invoke {@link Player#init(int, PlayerColor)}.
      * A new Board is created with specified board size and color is set to passed
      * playerColor. Also if view != null it's setViewer() method is called with the board.viewer() as argument and view.visualize()
      * is called.
-     * {@link #player}'s state is set depending on passed playerColor. If {@link PlayerColor#BLUE} is passed state is set to UPDATE,
+     * {@link Player}'s state is set depending on passed playerColor. If {@link PlayerColor#BLUE} is passed state is set to UPDATE,
      * if {@link PlayerColor#RED} is set to REQUEST.
      * @param size size of {@link Board} to play on.
      * @param color {@link PlayerColor} of player
