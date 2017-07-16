5d6398345dbd8ef3009a148da449b219518a55f0
robinwilliam.hundt <robinwilliam.hundt@stud.uni-goettingen.de>
2017-07-16 13:16:22 +0000

    Added comments for Task

--- src/towerwarspp/player/mcts/Task.java
+++ src/towerwarspp/player/mcts/Task.java
@@ -1,10 +1,19 @@
 package towerwarspp.player.mcts;
 
 /**
- * Created by robin on 15.07.17.
+ * Enum class whose fields represent states the MCTS  algorithm can be in.
  */
 public enum Task {
+    /**
+     * A new game has been started and the {@link Mcts} class has been notified to constrct a new tree.
+     */
     INIT,
+    /**
+     * Represents the state that a move has been played and the algorithm should from now on only considered
+     */
     MOVE_RECEIVED,
+    /**
+     * Represents the state that a move has been requested.
+     */
     MOVE_REQUESTED
 }
